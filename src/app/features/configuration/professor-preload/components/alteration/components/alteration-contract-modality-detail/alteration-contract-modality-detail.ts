97eef854899bb0636703c2448be530f094435090
DanielArias <dajo172020@gmail.com>
2026-09-23 08:21:37 -0500

    Correccion en alteration contract modality

--- src/app/features/configuration/professor-preload/components/alteration/components/alteration-contract-modality-detail/alteration-contract-modality-detail.ts
+++ src/app/features/configuration/professor-preload/components/alteration/components/alteration-contract-modality-detail/alteration-contract-modality-detail.ts
@@ -785,7 +785,6 @@ export class AlterationContractModalityDetail {
 
     this.isRequestingDeleteProfessor.set(true);
 
-    this.coordinationService
       this.coordinationService.getDeleteNovelties()
       .pipe(
         map((novelties) =>
